72593c74dd62267d67717ef0c8f9dc5c30b12399
Samantha Morales <120693377+TxtrisSammi@users.noreply.github.com>
2025-04-11 11:35:35 -0500

    File Service Readers Created

--- src/main/java/com/example/loginpage/Services/FileService.java
+++ src/main/java/com/example/loginpage/Services/FileService.java
@@ -1,13 +1,36 @@
 package com.example.loginpage.Services;
 
-public class FileService {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public static void readAllCSV(String filename) {
+
+public class FileService {
+    public static String[][] readAllCSV(String filename) throws FileNotFoundException {
+        File CSVFile = new File("src/main/resources/data/" + filename + ".csv");
+        Scanner read = new Scanner(CSVFile);
+
+        ArrayList<String[]> lines = new ArrayList<>();
+        for (String line = read.nextLine(); read.hasNextLine(); line = read.nextLine()) {
+            lines.add(line.split(","));
+        }
+
+        read.close();
+        return lines.toArray(new String[lines.size()][]);
     }
-    public static void readLineCSV(String filename) {
+    public static String[] readLineCSV(String filename) throws FileNotFoundException {
+        File CSVFile = new File("src/main/resources/data/" + filename + ".csv");
+        Scanner read = new Scanner(CSVFile);
+
+        String[] line = read.nextLine().split(",");
+        read.close();
+        return line;
     }
+
     public static void deleteRecordCSV(int id) {
     }
+
     public static void insertRecordCSV(int id, String[] data) {
     }
 
