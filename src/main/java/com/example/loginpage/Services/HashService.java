739a139ae9a14f5df70fc7bbec440b2a18f41b31
bpmryandinh <bdinh@my.okcu.edu>
2025-04-16 14:51:33 -0500

    fixed error
    user method in HashService

--- src/main/java/com/example/loginpage/Services/HashService.java
+++ src/main/java/com/example/loginpage/Services/HashService.java
@@ -49,9 +49,10 @@ public class HashService {
     }
 
     // Method to fill in data for user hashmap
+    // does the new String[]{userData[1]} work?
     public static void writeUserHashMap(String[][] UserSData){
         for(String[] userData : UserSData){
-            User user = new User(userData[0], userData[1], userData[2], userData[3], userData[4], userData[5], userData[6], userData[7]);
+            User user = new User(userData[0], new String[]{userData[1]}, userData[2], userData[3], userData[4], userData[5], userData[6], userData[7]);
             Users.put(Integer.parseInt(userData[0]), user);
         }
     }
